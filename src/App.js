5c438e29288ffa2fa8bc863c764240f16e6283b2
Lian Thompson <lianythompson@gmail.com>
2019-06-13 11:40:14 -0700

    add redux dependencies

--- src/App.js
+++ src/App.js
@@ -2,31 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Posts/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm>
-        <hr />
-      </PostForm>
-    </div>
-  );
-}
+const store = createStore(() => [], {}, applyMiddleware());
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <PostForm>
+            <hr />
+          </PostForm>
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
