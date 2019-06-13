49466548a3d54ec28d32faf391d29630d112d905
Lian Thompson <lianythompson@gmail.com>
2019-06-13 10:42:55 -0700

    add onChange for form

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 export default class Postform extends Component {
     constructor(props) {
         super(props);
-            this.state = {};
+        this.state = {
+            title: '',
+            body: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
     }
 
     render() {
@@ -14,12 +23,12 @@ export default class Postform extends Component {
                     <div>
                         <label>Title: </label>
                         <br />
-                        <input type="text" name="title"/>
+                        <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
                     </div>
                     <div>
                         <label>Body: </label>
                         <br />
-                        <textarea name="body" />
+                        <textarea name="body" onChange={this.onChange} value={this.state.body} />
                     </div>
                     <br />
                     <button type="submit">Submit</button>
